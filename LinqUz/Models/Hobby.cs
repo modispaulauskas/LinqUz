bd72abac9a9b7eefd4a650e300c084699000e311
Modestas Paulauskas <modispaulauskas@gmail.com>
2021-01-20 20:36:52 +0200

    Add project files.

---
+++ LinqUz/Models/Hobby.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqUz.Models
+{
+    class Hobby
+    {
+        public string text { get; set; }
+    }
+    class Profession
+    {
+        public string text { get; set; }
+
+    }
+}
---
+++ LinqUz/Models/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqUz.Models
+{
+    public class Person
+    {
+        public string firstname { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public DateTime? birthday { get; set; }
+        public string biology { get; set; }
+        public int? age
+        {
+            get
+            {
+                if(birthday == null) return null;
+                var timeSpan = DateTime.Now.Subtract((DateTime)birthday);
+                return new DateTime(timeSpan.Ticks).Year - 1;
+            }
+        }
+
+    }
+}
